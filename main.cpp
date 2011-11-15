0e9c52cc7d5d5bc75309ba4f8221269911aa40b8
jasdurable <b97901049@ntu.edu.tw>
2011-11-15 17:11:20 +0800

    11/15 5:10

--- main.cpp
+++ main.cpp
@@ -2,29 +2,32 @@
 #include <math.h>
 #include <stdlib.h>
 #include "End.h"
+#include "main.h"
 using namespace std;
+
+
+
 //lynn is here   jas try
 //hello world~
-//for parameter not changed!!
-int Simulation_Time = 1000;
-int MAX = 10;
-//
+
 //for parameter measuring the efficiency!!!
-int finish_time[100];
+int finish_time[EndNum];
 int access_time = 0;
 int collision_time = 0;
 //
 int _time = 1;// the order of the subframe
-int preamble[54] = {0};
+int preamble[preambleNum] = {0};
 int _count = 0;//to record the tutle number of successful and abort End
-void responseForUseSamePreamble();//for
+void responseForUseSamePreamble();
 // to set the probablity of successful receive, the input is the number of the restransmission
 int setProbablity( int );
+
+
 int main() {
-    End end[100];
+    End end[EndNum];
     srand(time(NULL));
-    for(; _count < 100 ; _time +=5 ){
-	for( int n = 0; n < 100; n++ )
+    for(; _count < EndNum ; _time +=5 ){
+	for( int n = 0; n < EndNum; n++ )
 	    end[n].setpreamble(); 
 	/*-----debug-----
 	for (int i = 0; i < 54; i++)
@@ -35,14 +38,14 @@ int main() {
 	for (int i = 0; i < 54; i++)
 	    cout << i << " : " << preamble[i] << endl;
 	//-----debug-----*/
-	for( int n = 0; n < 100; n++ ){
+	for( int n = 0; n < EndNum; n++ ){
 	    end[n].responseForOnlyOne();
 	    end[n].settime();
 	}
  
 	cout << _time << "\t" << _count << endl;
 	
-	for (int i = 0; i < 54; i++) {
+	for (int i = 0; i < preambleNum; i++) {
 	    preamble[i] = 0;
 	}
     }
@@ -51,7 +54,7 @@ int main() {
 }
 void responseForUseSamePreamble()
 {
-    for( int i = 0; i < 54;  i++ ){
+    for( int i = 0; i < preambleNum;  i++ ){
 	if( preamble[i] > 1 ){
 	//preamble[i] = setProbablity(1); 
 	    if (setProbablity(1) == 0) { 
