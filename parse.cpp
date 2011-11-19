96319f7cf7008cd5d6b7c5b0d1acf784acac4aa8
mouda <dses5211@gmail.com>
2011-11-20 00:01:36 +0800

    add parse file

---
+++ parse.cpp
@@ -0,0 +1,43 @@
+#include "parse.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void showHelp() {
+
+
+}
+void lineParsing (int argc, char** argv) {
+  if (argc == 1) {
+    showHelp();
+    exit(0);
+  }
+  else {
+
+    for (int i = 1; i < argc; i++) {
+      if (!strcmp(argv[i], "-help")) {
+        showHelp();
+        exit(0);
+      }
+      else if (!strcmp(argv[i], "-end"))
+        lte::EndNum = atoi(argv[++i]);
+      else if (!strcmp(argv[i], "-rand"))
+        lte::RandomBackoffIndex = atoi(argv[++i]);
+      else if (!strcmp(argv[i], "-type")) {
+        if (!strcmp(argv[++i], "uniform"))
+          lte::traffic_type = uniform_start;
+        else if (!strcmp(argv[i], "beta"))
+          lte::traffic_type = beta_start;
+        else {
+          cerr << "false traffic type " << argv[i] << endl;
+          exit(1);
+        }
+      }
+      else {
+        cerr << "false argument " << argv[i] << endl;
+        exit(1);
+      }
+    }
+
+  }
+}
---
+++ parse.h
@@ -0,0 +1,3 @@
+#include "main.h"
+using namespace std;
+void lineParsing(int argc, char** argv);
