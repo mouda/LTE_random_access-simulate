ed05e90a9623c5fd85d2f834cdff29ad30917383
jasdurable <b97901049@ntu.edu.tw>
2011-11-19 18:07:20 +0800

    new version by jas   11/19

--- End.h
+++ End.h
@@ -1,32 +1,17 @@
-//rxtern int _time;
-//extern int preamble[54];
-//extern int _count;
-//extern int MAX;
-//extern int setProbablity(int);
 class End {
   public:
-    End(){ _setTime = 1; _preamble = 0; _index = 1; _finishTime = 0; _collisionTime = 0;};
+    End(){ _setTime = 1; _preamble = 0; _index = 1; };
     void setpreamble();
     void responseForOnlyOne();
     void settime();
     void id();
-    int getCollision() { return _collisionTime;}
-    int getFinish() {return _finishTime;}
-    int getSettime() {return _setTime;}
+	void setStartTime(int t) {_setTime = t};
   private:
     int _setTime;
     int _preamble;
+    int _CountCeil(int t); //ceiling
+	
     // to record number of retransmission, _index = -1 means success
     // _index = -2 means abort
     int _index;
-    int _finishTime;
-    int _collisionTime;
-    int _CountCeil(int t); //ceiling
-
-    //int _setTime;
-    //int _preamble;
-
-    //int _CountCeil(int t); //ceiling
-	
-    //int _index;
 };
