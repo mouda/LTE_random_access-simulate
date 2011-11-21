106b69aadb8046ac32e565267e3b605346ddb446
jasdurable <b97901049@ntu.edu.tw>
2011-11-21 11:47:17 +0800

    small addition

--- End.h
+++ End.h
@@ -1,3 +1,8 @@
+//rxtern int _time;
+//extern int preamble[54];
+//extern int _count;
+//extern int MAX;
+//extern int setProbablity(int);
 class End {
   public:
     End(){ _setTime = 1; _preamble = 0; _index = 1; };
@@ -6,10 +11,11 @@ class End {
     void settime();
     void id();
 	void setStartTime(int t) {_setTime = t;}
+	int getStartTime() {return _setTime;}
   private:
     int _setTime;
     int _preamble;
-    int _CountCeil(int t); //ceiling
+	int _CountCeil(int t); //ceiling
 	
     // to record number of retransmission, _index = -1 means success
     // _index = -2 means abort
