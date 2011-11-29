bc49d438ff1fd66bf190b69f3612ae3767a5fc0a
mouda <dses5211@gmail.com>
2011-11-29 15:51:33 +0800

    new parameter stting & change the script run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-rm result.txt
 index=0;
-simulateTimes=20;
+simulateTimes=1;
 endNumber=100;
+nameForEndEvent="resultEnd_";
+nameForEndReport="reportEnd_";
+
+suffix="_.txt";
+EventFileName="resultEnd";
+ReportFileName="reportEnd";
+dirName="./result";
+
+rm *_.tx
+if [ ! -d $dirName ]; then
+  mkdir $dirName;
+fi 
+
 for index in $(seq 1 $simulateTimes)
 do 
   parameter=`expr 1000 + $index \* 2000`;
 #  echo $index;
+  EventFileName=${dirName}'/'${nameForEndEvent}${parameter}${suffix};
+  ReportFileName=${dirName}'/'${nameForEndReport}${parameter}${suffix};
   echo "==================== End number $parameter ======================";
-  ./simulate -end $parameter -rand 20 -type uniform >> result.txt
+  echo "EventFileName: $EventFileName";
+  ./simulate -end $parameter -rand 20 -type beta -max 10 -pre 10 >> ${EventFileName} 2>> ${ReportFileName};
 done
